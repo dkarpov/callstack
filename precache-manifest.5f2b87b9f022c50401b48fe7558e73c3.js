f6dd20d6b6e347a655395f8d709820c6a2e502f9
Dmytro Karpov <dimon.karpov1@gmail.com>
2019-05-08 01:41:06 +0200

    Updates

---
+++ precache-manifest.5f2b87b9f022c50401b48fe7558e73c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dd511b46fc4e4a28d6712f94203895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3417db6e7734524e79b9",
+    "url": "/static/css/main.fc0b9e51.chunk.css"
+  },
+  {
+    "revision": "4849cf8660185a5913fe",
+    "url": "/static/js/2.563df61d.chunk.js"
+  },
+  {
+    "revision": "3417db6e7734524e79b9",
+    "url": "/static/js/main.145f5ca3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
